7f9a6a96db8ec1d1c6ffaf2f62e67ccc0d98b15a
Julian Armstrong <julian.armstrong8@gmail.com>
2024-09-06 14:48:09 -0600

    included null check for method

--- PalindromeExercise/PalindromeExercise/WordSmith.cs
+++ PalindromeExercise/PalindromeExercise/WordSmith.cs
@@ -1,4 +1,4 @@
-using System;
+
 using System.Linq;
 
 namespace PalindromeExercise;
@@ -7,6 +7,10 @@ public class WordSmith
 {
     public bool IsAPalindrome(string word)
     {
+        if (string.IsNullOrEmpty(word))
+        {
+            return false;
+        }
         var reversedWord = new string(word.ToLower().Reverse().ToArray());
         return word.ToLower() == reversedWord;
     }
--- PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
+++ PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
@@ -1,4 +1,3 @@
-using System;
 using PalindromeExercise;
 using Xunit;
 
